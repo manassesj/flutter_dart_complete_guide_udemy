14558df8433f2f45dc7d113045b757da0de68681
Manassés júlio <jmanasses1024@hotmail.com>
2020-07-14 18:29:17 -0300

    .

---
+++ personal_expenses_app/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import './transactionModel.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Grocerioes',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text('CHART'),
+                elevation: 5.0,
+              ),
+            ),
+            Column(
+              children: transactions.map((transaction) {
+                return Card(
+                  child: Text(transaction.title),
+                );
+              }).toList(),
+            ),
+          ],
+        ));
+  }
+}
