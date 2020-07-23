af8439cdfa2b7a318ea0c25d492dfa11d73d722e
Manassés júlio <jmanasses1024@hotmail.com>
2020-07-23 19:52:08 -0300

    Registering a Screen as the Main Screen

--- meals_app/lib/categories_screen.dart
+++ meals_app/lib/categories_screen.dart
@@ -5,20 +5,25 @@ import 'package:meals_app/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map(
-            (category) => CategoryItem(
-              title: category.title,
-              color: category.color,
-            ),
-          )
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200.0,
-        childAspectRatio: 1.5,
-        crossAxisSpacing: 20.0,
-        mainAxisSpacing: 20.0,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (category) => CategoryItem(
+                title: category.title,
+                color: category.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200.0,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20.0,
+          mainAxisSpacing: 20.0,
+        ),
       ),
     );
   }
