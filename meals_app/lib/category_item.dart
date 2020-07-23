ee059f8faec6508dc0712d901f560335aae453e4
Manassés júlio <jmanasses1024@hotmail.com>
2020-07-23 20:25:19 -0300

    Navigating to a New Page

--- meals_app/lib/category_item.dart
+++ meals_app/lib/category_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import './dummy_data.dart';
+import 'package:meals_app/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -8,21 +7,36 @@ class CategoryItem extends StatelessWidget {
 
   const CategoryItem({this.title, this.color});
 
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealsScreen();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20.0),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color.withOpacity(0.7), color],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15.0),
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
         ),
-        borderRadius: BorderRadius.circular(15.0),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color.withOpacity(0.7), color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
       ),
     );
   }
---
+++ meals_app/lib/category_meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('The Recepies'),
+      ),
+      body: Center(
+        child: Text('The Recepies For the ...'),
+      ),
+    );
+  }
+}
