261855382696e257044a7a16e945e6dd25e96781
Manassés júlio <jmanasses1024@hotmail.com>
2020-07-30 22:02:14 -0300

    Managing Form Input Focus

--- shop_app/lib/screen/edit_product_screen.dart
+++ shop_app/lib/screen/edit_product_screen.dart
@@ -8,6 +8,8 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,17 +19,46 @@ class _EditProductScreenState extends State<EditProductScreen> {
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Form(
-          child: ListView(
-            children: <Widget>[
-              TextFormField(
-                
-                decoration: InputDecoration(
-                  labelText: 'Title',
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                buildInputForm(
+                    context: context,
+                    label: 'Title',
+                    onFieldSubmitted: (String value) {
+                      FocusScope.of(context).requestFocus(_priceFocusNode);
+                    }),
+                buildInputForm(
+                  context: context,
+                  label: 'Price',
+                  textBoardType: TextInputType.number,
+                  focusNode: _priceFocusNode,
                 ),
-                textInputAction: TextInputAction.next,
-              ),
-            ],
+              ],
+            ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Card buildInputForm({
+    BuildContext context,
+    String label,
+    TextInputType textBoardType = TextInputType.text,
+    FocusNode focusNode,
+    Function onFieldSubmitted,
+  }) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
+          decoration: InputDecoration(
+              labelText: label, hoverColor: Theme.of(context).primaryColor),
+          textInputAction: TextInputAction.next,
+          keyboardType: textBoardType,
+          focusNode: focusNode,
+          onFieldSubmitted: onFieldSubmitted,
         ),
       ),
     );
