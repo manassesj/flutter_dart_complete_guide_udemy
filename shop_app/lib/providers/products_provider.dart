0b98d7dd1bf1020fb079a42b518f9a4ce9ae8c81
Manassés júlio <jmanasses1024@hotmail.com>
2020-08-01 19:16:52 -0300

    Sending POST Requests

--- shop_app/lib/providers/products_provider.dart
+++ shop_app/lib/providers/products_provider.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -68,6 +71,17 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    const url = 'https://shopapp-7d462.firebaseio.com/products.json';
+
+    http.post(url,
+        body: json.encode({
+          'title': product.title,
+          'description': product.description,
+          'imageUrl': product.imageUrl,
+          'price': product.price,
+          'isFavorite': product.isFavorite,
+        }));
+
     final newProduct = Product(
       id: DateTime.now().toString(),
       price: product.price,
