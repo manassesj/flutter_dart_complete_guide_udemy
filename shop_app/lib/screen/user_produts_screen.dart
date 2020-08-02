9fefbe13a1cecba93fcfd49cd6a20cf620fff65a
Manassés júlio <jmanasses1024@hotmail.com>
2020-08-02 20:52:22 -0300

    How to Transform Fetched Data

--- shop_app/lib/screen/user_produts_screen.dart
+++ shop_app/lib/screen/user_produts_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/products_provider.dart';
-import 'package:shop_app/screen/edit_product_screen.dart';
-import 'package:shop_app/widgets/app_drawer.dart';
-import 'package:shop_app/widgets/user_product_item.dart';
+
+import '../providers/products_provider.dart';
+
+import '../screen/edit_product_screen.dart';
+
+import '../widgets/app_drawer.dart';
+import '../widgets/user_product_item.dart';
 
 class UserProductScreen extends StatelessWidget {
   static const routeName = '/User-product-screen';
+
+  Future<void> _refleshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,20 +23,24 @@ class UserProductScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () => Navigator.of(context).pushNamed(EditProductScreen.routeName),
+            onPressed: () =>
+                Navigator.of(context).pushNamed(EditProductScreen.routeName),
           )
         ],
       ),
       drawer: AddDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Consumer<Products>(
-          builder: (_, products, child) => ListView.builder(
-            itemCount: products.getItems.length,
-            itemBuilder: (_, index) => UserProductItem(
-              id: products.getItems[index].id,
-              title: products.getItems[index].title,
-              imageUrl: products.getItems[index].imageUrl,
+      body: RefreshIndicator(
+        onRefresh: () => _refleshProducts(context),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Consumer<Products>(
+            builder: (_, products, child) => ListView.builder(
+              itemCount: products.getItems.length,
+              itemBuilder: (_, index) => UserProductItem(
+                id: products.getItems[index].id,
+                title: products.getItems[index].title,
+                imageUrl: products.getItems[index].imageUrl,
+              ),
             ),
           ),
         ),
